1a5a42107c88bfaf75c7a046721a44806b061184
AngelIsABoy <liyakai@mail.ustc.edu.cn>
2015-05-04 22:37:03 +0800

    accept a istream& and return a istream&,print the data

---
+++ 8_IO/8.1.c
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+
+istream& stream1(istream& stream){
+    char word;
+    while(stream >> word){
+        cout<<word<<endl;
+    };
+    stream.clear();
+}
+
+int main(){
+    stream1(cin);
+    cout<<"This is the end of program."<<endl;
+}
