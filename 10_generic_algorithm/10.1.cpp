f965af5f6bcb424fcff48a3af792f83517f62935
AngelIsABoy <liyakai@mail.ustc.edu.cn>
2015-05-11 19:47:09 +0800

    count the number of the sequence by count().

---
+++ 10_generic_algorithm/10.1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> ve;
+    int N, M;
+    int i;
+    cout<<"Please input the number of int and the number you want to count."<<endl;
+    cin>>N>>M;
+    while(N--){
+        cin >> i;
+        ve.push_back(i);
+    }
+    cout<<"The number of "<<M<<" is:"<<count(ve.cbegin(), ve.cend(), M)<<endl;
+}
